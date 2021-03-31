7b5bd7d1e643bbe4abc0cb994cc620691fe3b74e
Nazanin Saeidi <71193970+nazanin-saeidi@users.noreply.github.com>
2021-03-31 10:27:15 +0430

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,3 @@
+<?php
+    define('APP_ROOT', dirname(dirname(__FILE__)));
+    define('APP_URL', 'http://localhost/phpmvc');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,10 +1,19 @@
 <?php
     class Pages extends Controller {
         public function __construct() {
+            
         }
 
         public function index() {
-            $this->view('Hello');
+            $data = [
+                'title' => 'Welcome to My phpmvc application!'
+            ];
+
+            $this->view('pages/index', $data);
+        }
+
+        public function about() {
+            $this->view('pages/about');
         }
 
     }
